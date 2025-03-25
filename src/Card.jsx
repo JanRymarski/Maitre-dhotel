4631b91d1d50bfcb96d4442eb78a59a0c92eea09
JanRymarski <jan.rymarski@student.howest.be>
2025-03-25 17:14:10 +0100

    First table component created

---
+++ src/Card.jsx
@@ -0,0 +1,10 @@
+function Card(){
+    return(
+        <div className="card">
+            <h2 className="card-title">Tafel 1</h2>
+            <p className="card-text">Hier moet een menu komen</p>
+        </div>
+    )
+}
+
+export default Card;
