bead38c663afef419cb51bf95abdde3b3e551670
JanRymarski <jan.rymarski@student.howest.be>
2025-03-26 17:08:53 +0100

    Logica of menu update

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,19 +3,14 @@ import List from "./List";
 
 function Card(props) {
     const [isBezet, setIsBezet] = useState(props.isBezet);
-    const [showList, setShowList] = useState(false); 
 
     const toggleBezet = () => {
         setIsBezet(!isBezet); 
     };
 
-    const toggleList = () => {
-        setShowList(!showList); 
-    };
-
     return (
         <div className="card">
-            <h2 className="card-title"> Tafel {props.number}</h2>
+            <h2 className="card-title">Tafel {props.number}</h2>
             <p className="card-text">Hier moet een menu komen</p>
             <p 
                 className={`card-text ${isBezet ? "bezet" : "vrij"}`} 
@@ -25,11 +20,7 @@ function Card(props) {
                 {isBezet ? "Bezet" : "Vrij"}
             </p>
             
-            <button onClick={toggleList}>
-                {showList ? "Verberg Lijst" : "Toon Lijst"}
-            </button>
-            {showList && <List />}
-            
+            {isBezet && <List />}
         </div>
     );
 }
